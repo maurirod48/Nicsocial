14aa8165c0de335a16d60fe009b9611b855ae636
detonator <maurirodriguezm48@gmail.com>
2026-04-27 21:52:58 +0000

    login signup page media queries

--- resources/views/signin_login/login.blade.php
+++ resources/views/signin_login/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MDM Login</title>
+    <title>Nicsocial</title>
     @vite('resources/js/app.js')
 </head>
 <body class="login-page">
