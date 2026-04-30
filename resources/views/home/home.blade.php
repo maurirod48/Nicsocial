7eceb0b8700c7d03ae05166084b0b3216a873d7a
detonator <maurirodriguezm48@gmail.com>
2026-04-30 18:54:39 +0000

    created tabs for home feed section

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,5 +2,27 @@
 
 
 @section('content')
-<h1>Home page</h1>
+
+
+<div class="home-main-container">
+    <div class="home-middle-section-container">
+        <div class="feed-tabs-wrapper">
+            <label for="global" class="feed-tab">
+                <h1>Public</h1>
+                <input type="radio" id="global" name="hh" class="feed-tab-radio">
+                <span class="public-tab-bottom-line"></span>
+            </label>
+            <label for="friends-only" class="feed-tab">
+                <h1>Friends</h1>
+                <input type="radio" id="friends-only" name="hh" class="feed-tab-radio">
+                <span class="friends-tab-bottom-line"></span>
+            </label>
+        </div>
+        <div class="dynamic-feed-section">
+            {{-- JS inserts posts here dynamically depending on the seletec tab --}}
+        </div>
+    </div>
+</div>
+
+
 @endsection
