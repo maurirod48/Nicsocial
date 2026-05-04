af1820579258fd90a4a446034153d174952161d5
detonator <maurirodriguezm48@gmail.com>
2026-05-04 21:21:51 +0000

    like btn changes if post is liked by logged in user

--- resources/js/home_js_files/home.js
+++ resources/js/home_js_files/home.js
@@ -94,7 +94,7 @@ function displayPublicPosts(posts) {
 
                 <div class="post-footer">
                     <div class="post-react-btn-container">
-                        <img alt="like button" src="/images/website_images/like_btn.png" class="post-like-btn post-btn">
+                        <img alt="like button" src="/images/website_images/${post.likedByYou ? 'liked.png' : 'like_btn.png'}" class="post-like-btn post-btn">
                         <img alt="like button" src="/images/website_images/dislike_btn.png" class="post-dislike-btn post-btn">
                         <img alt="like button" src="/images/website_images/share_btn.png" class="post-share-btn post-btn">
                     </div>
@@ -107,36 +107,48 @@ function displayPublicPosts(posts) {
                 
             `;
         } else if (!postCard.image) {
-            postCard.innerHTML = `
-                <input type="hidden" value=${post.id} class="post-id">
-                <input type="hidden" value=${post.user.id} class="user-id">
-                <div class="post-header">
-                    <div class="user-info-container">
-                        <img src="https://nicsocial-images.s3.us-east-2.amazonaws.com/images/other_images/${post.user.profile_pic_path}"
-                        class="user-img">
 
-                        <div>
-                            <h3 class="post-creator-name">${post.user.name}</h3>
-                            <p class="post-creator-email">${post.user.email}</p>
+                postCard.innerHTML = `
+                    <input type="hidden" value=${post.id} class="post-id">
+                    <input type="hidden" value=${post.user.id} class="user-id">
+                    <div class="post-header">
+                        <div class="user-info-container">
+                            <img src="https://nicsocial-images.s3.us-east-2.amazonaws.com/images/other_images/${post.user.profile_pic_path}"
+                            class="user-img">
+
+                            <div>
+                                <h3 class="post-creator-name">${post.user.name}</h3>
+                                <p class="post-creator-email">${post.user.email}</p>
+                            </div>
                         </div>
+
+                        <div class="post-options-container">
+                            <img src="/images/website_images/three_dots.png" alt="post-options-image" class="post-options-image">
+                        </div>
+                        
                     </div>
 
-                    <div class="post-options-container">
-                        <img src="/images/website_images/three_dots.png" alt="post-options-image" class="post-options-image">
+                    <div class="post-body">
+                        <h1 class="post-title">${post.title}</h1>
+                        <div class="post-description">
+                            <p>
+                                ${post.description}
+                            </p>
+                        </div>
+
                     </div>
                     
-                </div>
-
-                <div class="post-body">
-                    <h1 class="post-title">${post.title}</h1>
-                    <div class="post-description">
-                        <p>
-                            ${post.description}
-                        </p>
+                    <div class="post-footer">
+                    <div class="post-react-btn-container">
+                        <img alt="like button" src="/images/website_images/${post.likedByYou ? 'liked.png' : 'like_btn.png'}" class="post-like-btn post-btn">
+                        <img alt="like button" src="/images/website_images/dislike_btn.png" class="post-dislike-btn post-btn">
+                        <img alt="like button" src="/images/website_images/share_btn.png" class="post-share-btn post-btn">
                     </div>
-
-                </div>
-                
+                    <div class="post-stats-container">
+                        <p> likes ${post.likes}</p>
+                        <p> dislikes ${post.dislikes}</p>
+                        <p>shared ${post.times_shared}</p>
+                    </div>
             `;
         }
 
@@ -213,7 +225,7 @@ function likePost(postId) {
     })
     .then(data => {
         if (data.success) {
-            location.reload();
+            getPublicPosts();
             console.log('Post current like count', data.post.likes);
             console.log(data.message);
         } 
