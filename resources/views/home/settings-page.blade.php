d46f7c1c349ca3746755d2eb4d445c007287dda0
detonator <maurirodriguezm48@gmail.com>
2026-05-11 21:56:01 +0000

    still working on settings section

--- resources/views/home/settings-page.blade.php
+++ resources/views/home/settings-page.blade.php
@@ -5,7 +5,10 @@
 
 <div class="settings-section-wrapper">
     <div class="settings-section-container">
-        
+        <div class="change-password-option option">
+            <p>Change password</p>
+            <img src="{{ asset('images/website_images/arrow.png') }}" alt="arrow image" class="arrow-icon">
+        </div>
     </div>
 </div>
 
